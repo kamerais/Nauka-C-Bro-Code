96bc3a78a1f4d21d7d52315dec87a98f5d6a069b
kamerais <kubickimarcin102@gmail.com>
2024-02-15 20:09:47 +0100

    Dodałem jak tworzy się komentarze oraz przykład zastosowania sekwencji wyjściowych

--- C/01. comments & escape sequences/main.c
+++ C/01. comments & escape sequences/main.c
@@ -3,6 +3,24 @@
 int main(){
     
     // To jest komentarz jednolinijkowy
-    
+    /*
+        To
+        jest
+        komentarz
+        wielolinijkowy
+    */
+    /*
+        escape sequences (sekwencje wyjścia) = kombinacja znaków zawierających backslash \
+                                               po którym jest literka lub kombinacja cyfr.
+                                               Specyfikują akcje wewnątrz linijki lub stringu tekstu
+                                               \n = nowa linia (newline)
+                                               \t = wstaw tabulator (taby)
+                                               \\ = wyświetla \
+                                               \' = wyświetla '
+                                               \"" = wyświetla ""
+    */
+
+    printf("1\t2\t3\n4\t5\t6\n7\t8\t9");
+    printf("\"I like Pizza\"- Abraham Lincoln probably");
     return 0;
 }
