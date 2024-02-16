650aeae870e6ac45da15feac51b2cbe3f4e60a01
kamerais <kubickimarcin102@gmail.com>
2024-02-16 16:44:08 +0100

    Definicja i przykłady specyfikatorów formatu

--- C/04. format specifiers/main.c
+++ C/04. format specifiers/main.c
@@ -2,5 +2,25 @@
 
 int main(){
     
+    // format specifier % = definiuje i formatuje typ danych, który ma się wyświetlić
+
+    // %c = character
+    // %s = string
+    // %f = float
+    // %lf = double
+    // %d = decimal
+
+    // %.1 = decimal precision
+    // %1 = minimum field width
+    // %- = left align
+
+    float item1 = 5.75;
+    float item2 = 10.00;
+    float item3 = 100.99;
+
+    printf("Item 1: $%-8.2f\n", item1);
+    printf("Item 2: $%-8.2f\n", item2);
+    printf("Item 3: $%-8.2f\n", item3);
+
     return 0;
 }
