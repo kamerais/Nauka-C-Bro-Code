768aefbc3a0491e6d1adc56e940f14987079ec38
kamerais <kubickimarcin102@gmail.com>
2024-02-16 17:58:35 +0100

    Przykłady metod dla user input +lekko zaawansowane rzeczy

--- C/07. user input/main.c
+++ C/07. user input/main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include <string.h> // korzstam z biblioteki string, bo potrzebuję metody do usunięcia nowej linii przy fgets
 
 int main(){
     
+    int age;
+    char name[25]; // 25 bitów
+
+    printf("What is your name? ");
+    //scanf("%s", &name); // & = wskazuje na adres w pamięci
+    fgets(name, 25, stdin); // korzystamy z fget, jeżeli w inpucie mają występować spacje
+    name[strlen(name)-1] = '\0'; // to jest po to, aby usunąć nową linię przy korzystanoi z fgets
+    
+    printf("How old are you? ");
+    scanf("%d",&age);
+
+    printf("Hello %s, how are you?\n", name);
+    printf("You are %d years old", age);
     return 0;
 }
