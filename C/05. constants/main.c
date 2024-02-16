66cdf9c0563897f671d973fb3c37d113edd18ca3
kamerais <kubickimarcin102@gmail.com>
2024-02-16 17:09:22 +0100

    Definicja stałej i przykład zastosowania

--- C/05. constants/main.c
+++ C/05. constants/main.c
@@ -2,5 +2,25 @@
 
 int main(){
     
+    // constant (stała) = jest to po prostu wartość, której nie można zamienić podczas wykonywania programu
+
+    float pi = 3.14159;
+
+    printf("pi = %f\n",pi); // output: 3.14159
+
+    // mogę przypadkiem nadpisać zmienną pi
+    pi = 31.4159;
+
+    printf("pi = %f (po nadpisaniu)\n",pi); // output: 31.4159
+
+    const float PI = 3.14159;
+
+    printf("PI = %f\n", PI); // output: 3.14159
+
+    // spróbuję nadpisać PI
+
+    //PI = 31.4159; nie można nadpisać wyskakuje błąd
+
+
     return 0;
 }
