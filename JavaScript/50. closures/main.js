1569497eb024734bebf9156fc21ac2037b23986e
kamerais <kubickimarcin102@gmail.com>
2024-02-19 22:44:00 +0100

    Definicja closures

---
+++ JavaScript/50. closures/main.js
@@ -0,0 +1,5 @@
+// closure = Funkcja zdefiniowana wewnątrz innej funkcji,
+//           wewnętrzna funkcja ma dostęp do zmiennych
+//           i zakresu zewnętrznej funkcji
+//           Pozwala na prywatne zmienne i utrzymanie stanu
+//           używanego często w JS frameworkach: React, Vue, Angular
