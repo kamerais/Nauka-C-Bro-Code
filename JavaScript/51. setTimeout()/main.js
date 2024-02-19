dec6add67cf56bc710749f9243243d4917f0aa9b
kamerais <kubickimarcin102@gmail.com>
2024-02-19 23:17:20 +0100

    Definicja setTimeout()

---
+++ JavaScript/51. setTimeout()/main.js
@@ -0,0 +1,6 @@
+// setTimeout() = funkcja w JavaScripcie, która pozwala na zaplanowanie
+//                wykonania funkcji po danej ilości czasu (milisekundy)
+//                Czasy są przybliżane (w zależności zależą od workload czasu uruchamiania środowiska JavaScript)
+
+//                setTimeout(callback, delay);
+
