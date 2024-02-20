06a66ccfb091026cd6b60c17882a619e0bc6feb8
kamerais <kubickimarcin102@gmail.com>
2024-02-20 14:46:23 +0100

    Poprawianie logiki programu

--- JavaScript/53. stopwatch program/main.js
+++ JavaScript/53. stopwatch program/main.js
@@ -6,7 +6,7 @@ let isRunning = false;
 
 function start(){
     if(!isRunning){
-        startTime = Date.now - elapsedTime;
+        startTime = Date.now() - elapsedTime;
         timer = setInterval(update, 10);
         isRunning = true;
     }
@@ -22,15 +22,15 @@ function stop(){
 
 function reset(){
     clearInterval(timer);
-    let startTime = 0;
-    let elapsedTime = 0;
-    let isRunning = false;
+    startTime = 0;
+    elapsedTime = 0;
+    isRunning = false;
     display.textContent = "00:00:00:00";
 }
 
 function update(){
-    const currentTimer = Date.now();
-    elapsedTime = currentTimer - startTime;
+    const currentTime = Date.now();
+    elapsedTime = currentTime - startTime;
 
     let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
     let minutes = Math.floor(elapsedTime / (1000 * 60) % 60);
@@ -38,9 +38,9 @@ function update(){
     let milliseconds = Math.floor(elapsedTime % 1000 / 10);
 
     hours = String(hours).padStart(2, "0");
-    minutes = String(minutes).padStart(2, "0")
-    seconds = String(seconds).padStart(2, "0")
-    milliseconds = String(milliseconds).padStart(2, "0")
+    minutes = String(minutes).padStart(2, "0");
+    seconds = String(seconds).padStart(2, "0");
+    milliseconds = String(milliseconds).padStart(2, "0");
 
-    display.textContent = `${hours}:${minutes}:${seconds}:${milliseconds}`
+    display.textContent = `${hours}:${minutes}:${seconds}:${milliseconds}`;
 }
