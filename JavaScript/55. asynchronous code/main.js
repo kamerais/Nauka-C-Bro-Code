c0b015487e4136234fa0710c63ee48f6c9722a0f
kamerais <kubickimarcin102@gmail.com>
2024-02-20 15:06:18 +0100

    Definicja kodu asynchronicznego

---
+++ JavaScript/55. asynchronous code/main.js
@@ -0,0 +1,7 @@
+// synchronous = Wykonuje konsekwentnie linijka po linijce w sposób sekwencyjny
+//               Kod, który aż operacja się skończy
+
+// asynchronous = Pozwala na zwielokrotnienie operacji, aby wykonywały się jednocześnie bez czekania
+//                Nie blokuje płynności wykonywania i pozwala programowi kontynuować
+//                (operacje I/O,  wymagania sieciowe, dostawanie danych)
+//                Obsługiwane przez: Callbacks, Promises, Async/Await
