86723cb1c2c63e61c21591035b72fc56bac36cff
kamerais <kubickimarcin102@gmail.com>
2024-02-20 15:27:08 +0100

    Przykłady błędów i wyjaśnienie metod do ich obsługi

--- JavaScript/56. error handling/main.js
+++ JavaScript/56. error handling/main.js
@@ -1,2 +1,14 @@
 // Error = Jest to obiekt stworzony do reprezentacji problemu, który wystąpił.
 //         Występuje często przy User Input lub ustaleniu połączenia
+
+// try { } = Zamyka kod tam, gdzie może wystąpić error
+// catch { } = Łapie i obsługuje jakikolwiek error z try { }
+// finally { } = (opcjonalne) Zawsze wykonywany. Używany głównie do czyszczenia.
+//               np. zamykanie plików, zamykanie połączenia, zwalnianie zasobów
+
+
+// Przykłady błędów
+
+console.log(x); // output: Uncaught ReferenceError: x is not defined
+
+console.lag("Hello"); // output: Uncaught TypeError: console.lag is not a function
