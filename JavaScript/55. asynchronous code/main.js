bb33cc4aacff6d40bae97c3fbcc6cdfdf76e6a7c
kamerais <kubickimarcin102@gmail.com>
2024-02-20 15:15:24 +0100

    Przykład kodów synchronicznego i asynchronicznego oraz obsługa kodu asynchronicznego

--- JavaScript/55. asynchronous code/main.js
+++ JavaScript/55. asynchronous code/main.js
@@ -5,3 +5,31 @@
 //                Nie blokuje płynności wykonywania i pozwala programowi kontynuować
 //                (operacje I/O,  wymagania sieciowe, dostawanie danych)
 //                Obsługiwane przez: Callbacks, Promises, Async/Await
+
+// Przykład kodu asynchronicznego
+
+// setTimeout(() => console.log("Task 1"), 3000);
+
+// Przykład kodu synchronicznego
+// console.log("Task 2");
+// console.log("Task 3");
+// console.log("Task 4");
+
+// Wyświetlanie Task 2, Task 3, Task 4 wykona się pierwsze, a Task 1 będzie na końcu
+
+// Przykład obsługi asynchronicznego kodu
+
+function func1(callback){
+    setTimeout(() => {console.log("Task 1");
+                      callback()}, 3000);
+}
+
+function func2(){
+    console.log("Task 2");
+    console.log("Task 3");
+    console.log("Task 4");
+}
+
+func1(func2);
+
+// Teraz kod wykonuje się poprawnie i Task 1 jest pierwszy, a nie ostatni
