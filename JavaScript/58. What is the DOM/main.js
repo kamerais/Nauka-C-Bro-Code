440278bc93c4a287daacf4c0abd6eb1a2abc0338
kamerais <kubickimarcin102@gmail.com>
2024-02-20 23:50:39 +0100

    Definicja DOM (Document Object Model)

---
+++ JavaScript/58. What is the DOM/main.js
@@ -0,0 +1,7 @@
+// DOM = Document Object Model
+//       Object(), który reprezentuje strone, którą widzisz w przeglądarce internetowej
+//       i dostarcza ci API do interaktowania z nim.
+//       Przeglądarka internetowa konstruuje DOM kiedy ładuje dokument HTML
+//       oraz strukturyzuje wszystle elementy w ala drzewie reprezentacji.
+//       JavaScript może korzystać z DOM do dynamicznego
+//       zmiania zawartości, struktury i stylu na stronie internetowej
