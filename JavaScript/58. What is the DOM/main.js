454b15a5fd826319c74dcc82d3ee31f709e141b0
kamerais <kubickimarcin102@gmail.com>
2024-02-21 00:00:09 +0100

    Przykłady metod z objektu DOM i ich zastosowanie

--- JavaScript/58. What is the DOM/main.js
+++ JavaScript/58. What is the DOM/main.js
@@ -5,3 +5,20 @@
 //       oraz strukturyzuje wszystle elementy w ala drzewie reprezentacji.
 //       JavaScript może korzystać z DOM do dynamicznego
 //       zmiania zawartości, struktury i stylu na stronie internetowej
+
+console.dir(document); // wyświela wszystkie metody obiektu document
+
+// mogę zmienić tytuł mojej strony
+
+document.title = "My website";
+
+// bez korzystania z CSSa mogę zmienić wygląd mojej strony
+
+document.body.style.backgroundColor = "hsl(0,0%,15%)"; // zmienia kolor tła na czarny
+
+// mogę dodać wartość dla wyświetlanej wartości
+const username = "kamerais";
+const welcomeMsg = document.getElementById("welcome-msg");
+
+welcomeMsg.textContent += username === "" ? 'Guest' : username;
+
