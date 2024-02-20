086b8b947b877ba682ac277c634e8db57f1cb8d5
kamerais <kubickimarcin102@gmail.com>
2024-02-20 14:51:40 +0100

    Definicja ES6 Modules

---
+++ JavaScript/54. ES6 Modules/main.js
@@ -0,0 +1,5 @@
+// ES6 Module = Zewnętrzny plik posiadający reużywalny kod
+//              który może być zainportowany wewnątrzn innego pliku JavaScript.
+//              Można pisać reużywalny kod dla wielu różnych aplikacji
+//              Może zawierać zmienne, klasy, funkcje ... i wiele innych.
+//              Wprowadzony wraz z aktualizacją ECMAScript 2015
