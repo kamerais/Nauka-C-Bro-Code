261cb041e00147b8036ae4c7ac2f0073e1e1d975
kamerais <kubickimarcin102@gmail.com>
2024-02-20 14:59:32 +0100

    Przykład użycia ES6 Modules

--- JavaScript/54. ES6 Modules/main.js
+++ JavaScript/54. ES6 Modules/main.js
@@ -3,3 +3,18 @@
 //              Można pisać reużywalny kod dla wielu różnych aplikacji
 //              Może zawierać zmienne, klasy, funkcje ... i wiele innych.
 //              Wprowadzony wraz z aktualizacją ECMAScript 2015
+
+import {PI, getCircumference, getArea, getVolume} from './mathUtil.js';
+
+console.log(PI);
+const circumference = getCircumference(10);
+
+console.log(`${circumference.toFixed(2)}cm`);
+
+const area = getArea(10);
+
+console.log(`${area.toFixed(2)}cm^2`);
+
+const volume = getVolume(10);
+
+console.log(`${volume.toFixed(2)}cm^3`);
