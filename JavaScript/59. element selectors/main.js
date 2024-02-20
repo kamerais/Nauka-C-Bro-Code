c4b7279646f56f648d775478e3363746848fabb1
kamerais <kubickimarcin102@gmail.com>
2024-02-21 00:04:33 +0100

    Definicja element selektorów i ich przykłady

---
+++ JavaScript/59. element selectors/main.js
@@ -0,0 +1,9 @@
+// element selectors = Metody używane do celowania w oraz manipulowania elementami HTMl.
+//                     Pozwalają na wybranie jednego lub kilku elementów HTML
+//                     z DOM (Document Object Model)
+
+// 1. document.getElementById()         // ELEMENT lub NULL
+// 2. document.getElementsClassName()   // KOLEKCJA HTML
+// 3. document.getElementsByTagName()   // KOLEKCJA HTML
+// 4. document.querySelector()          // ELEMENT LUB NULL
+// 5. document.querySelectorAll()       // NODELIST
