68d890a06c681c4300eafa28d03dcbac1a18e755
kamerais <kubickimarcin102@gmail.com>
2024-02-23 22:13:09 +0100

    Revert "Definicja classList"
    This reverts commit 07617e8126533b06529f968aaf88a2071b31aa84.

--- JavaScript/66. classList/main.js
+++
@@ -1,4 +0,0 @@
-// classList = Własność elementu w JS używany do interakcji
-//             z elementem z listy klas (CSS classes)
-//             Pozwala na zrobienie reużywalnych klas dla wielu elementów
-//             przez twoją stronę internetową
