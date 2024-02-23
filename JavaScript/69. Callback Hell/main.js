0cfc2f04fe2cee43c03484798d5e601ef8512ae5
kamerais <kubickimarcin102@gmail.com>
2024-02-23 23:32:39 +0100

    Definicja callback hell

---
+++ JavaScript/69. Callback Hell/main.js
@@ -0,0 +1,5 @@
+// Callback Hell = Sytuacja w JS gdzie callback-i
+//                 są zagnieżdzone wewnątrz innych callback-ów do takiego
+//                 stopnia gdzie kod jest trudny do czytania
+//                 Stary schemat do radzenia sobie z funkcjami asynchronicznymi
+//                 Używaj z obietnic + async/await aby uniknąć callback hell
