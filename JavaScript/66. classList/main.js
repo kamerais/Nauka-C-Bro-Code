8c525ba79dd3c9c124f5db979a5f12274177c160
kamerais <kubickimarcin102@gmail.com>
2024-02-23 22:15:09 +0100

    Definicja classList i jej przykładowe metody

---
+++ JavaScript/66. classList/main.js
@@ -0,0 +1,10 @@
+// classList = Własność elementu w JS używany do interakcji
+//             z elementem z listy klas (CSS classes)
+//             Pozwala na zrobienie reużywalnych klas dla wielu elementów
+//             przez twoją stronę internetową
+
+// add()
+// remove()
+// toggle(usuń jeżeli istnieje, dodaj jeżeli nie)
+// replace(staraKlasa, nowaKlasa)
+// contains()
