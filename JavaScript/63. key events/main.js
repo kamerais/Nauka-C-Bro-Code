95fa94757592137f55d612dc5e38c8489e8ce8dc
kamerais <kubickimarcin102@gmail.com>
2024-02-23 20:56:04 +0100

    Definicja eventListener

---
+++ JavaScript/63. key events/main.js
@@ -0,0 +1,3 @@
+// eventListener = słucha po specjalną akcję do tworzenia interaktywnych stron internetowych
+//                 akcje: keydown, keyup
+//                 .addEventListener(event, callback)
