3ec5133a2f3848a125ca213f32a5ddb73680e4dd
kamerais <kubickimarcin102@gmail.com>
2024-02-24 00:24:01 +0100

    Definicje Async/Await

---
+++ JavaScript/71. async await/main.js
@@ -0,0 +1,6 @@
+// Async/Await = Async = tworzy funkcję, która zwraca obietnicę
+//               Await = tworzy asynchroniczną funkcję, która oczekuje na obietnicę
+
+//               Pozwala na napisanie asynchronicznego kodu w sposób synchroniczny
+//               Async nie posiada parametrów zatwierdzenia lub odrzucenia
+//               Wszystkie po Await jest umieszczone w kolejce zdarzeń
