0488f9edb460522a941fe9e37a46159a713194f7
kamerais <kubickimarcin102@gmail.com>
2024-02-24 00:59:41 +0100

    Definicja fetch

---
+++ JavaScript/73. Fetch data from an API/main.js
@@ -0,0 +1,5 @@
+// fetch = Funkcja używana do robienia HTTP requestów do wydobycia zasobów.
+//         (JSON style data, images, files)
+//         Upraszcza asynchronicznie wydobywanie danych w JS i
+//         danych asynchronicznie poprzez sieć.
+//         fetch(url, (options))
