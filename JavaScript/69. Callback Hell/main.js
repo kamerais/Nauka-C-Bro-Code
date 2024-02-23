74c01f8392b5a2ab1784d733fd4840ffc4728733
kamerais <kubickimarcin102@gmail.com>
2024-02-23 23:40:46 +0100

    Przykład Callback Hell

--- JavaScript/69. Callback Hell/main.js
+++ JavaScript/69. Callback Hell/main.js
@@ -3,3 +3,37 @@
 //                 stopnia gdzie kod jest trudny do czytania
 //                 Stary schemat do radzenia sobie z funkcjami asynchronicznymi
 //                 Używaj z obietnic + async/await aby uniknąć callback hell
+
+function task1(callback){
+    setTimeout(() =>{
+        console.log("Task 1 complete");
+        callback()
+    }, 2000);
+    
+}
+function task2(callback){
+    setTimeout(() =>{
+        console.log("Task 2 complete");
+        callback()
+    }, 1000);
+}
+function task3(callback){
+    setTimeout(() =>{
+        console.log("Task 3 complete");
+        callback()
+    }, 3000);
+}
+function task4(callback){
+    setTimeout(() =>{
+        console.log("Task 4 complete");
+        callback()
+    }, 1500);
+}
+
+task1(() =>{
+    task2(() =>{
+        task3(() =>{
+            task4(() => console.log("All task complete"));
+        });
+    });
+});
