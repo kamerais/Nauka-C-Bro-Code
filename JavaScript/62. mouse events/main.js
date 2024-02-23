945eebefc2a90f3381222288f921ee042fe4e593
kamerais <kubickimarcin102@gmail.com>
2024-02-23 20:38:22 +0100

    Definicja eventListener

---
+++ JavaScript/62. mouse events/main.js
@@ -0,0 +1,3 @@
+// eventListener = słucha po specjalną akcję do tworzenia interaktywnych stron internetowych
+//                 akcje: click, mouseover (najechać), mouseout (odjechać)
+//                 .addEventListener(event, callback)
