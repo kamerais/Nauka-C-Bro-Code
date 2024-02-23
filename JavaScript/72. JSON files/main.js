329fb6ef46515eda5582d8691fe1fc7786c32ceb
kamerais <kubickimarcin102@gmail.com>
2024-02-24 00:56:05 +0100

    Tworzenie przykładowych plików typu JSON oraz zastosowanie JSONa

--- JavaScript/72. JSON files/main.js
+++ JavaScript/72. JSON files/main.js
@@ -4,3 +4,28 @@
 
 //        JSON.stringify() = konwertuje obiekt JS na JSON string
 //        JSON.parse() = konwertuje JSON string na obiekt JS
+
+const names = ["Spongebob", "Patrick", "Squidward", "Sandy"];
+const person = {"name": "Spongebob", "age": 30,"isEmployed": true,"hobbies": ["Jellyfishing", "Karate", "Cooking"]};
+const people = [{"name": "Spongebob", "age": 30,"isEmployed": true},
+                {"name": "Patrick", "age": 34, "isEmployed": false},
+                {"name": "Squidward","age": 50,"isEmployed": true},
+                {"name": "Sandy", "age": 27, "isEmployed": false}];
+
+// const jsonString = JSON.stringify(names); // zamienia na tekst
+
+const jsonNames = `["Spongebob", "Patrick", "Squidward", "Sandy"]`;
+const jsonPerson = `{"name": "Spongebob", "age": 30,"isEmployed": true,"hobbies": ["Jellyfishing", "Karate", "Cooking"]}`;
+const jsonPeople = `[{"name": "Spongebob", "age": 30,"isEmployed": true},
+                {"name": "Patrick", "age": 34, "isEmployed": false},
+                {"name": "Squidward","age": 50,"isEmployed": true},
+                {"name": "Sandy", "age": 27, "isEmployed": false}]`;
+
+const parseData = JSON.parse(jsonPeople);
+
+// Dodatkowo jak wydobyć dane z JSONa
+
+fetch("person.json")
+    .then(response => response.json()) // zwraca obietnice
+    .then(values => values.forEach(value => console.log(value.isEmployed)))
+    .catch(error => console.error(error));
