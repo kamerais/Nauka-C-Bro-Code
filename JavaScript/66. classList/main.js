73b4b8739cbd5d320551951f925e901800787004
kamerais <kubickimarcin102@gmail.com>
2024-02-23 22:32:31 +0100

    Przykłady zastosowań classList

--- JavaScript/66. classList/main.js
+++ JavaScript/66. classList/main.js
@@ -8,3 +8,31 @@
 // toggle(usuń jeżeli istnieje, dodaj jeżeli nie)
 // replace(staraKlasa, nowaKlasa)
 // contains()
+
+const myButton = document.getElementById("myButton");
+
+// myButton.classList.add("enabled");
+// myButton.classList.remove("enabled");
+
+// myButton.addEventListener("mouseover", event =>{
+//     event.target.classList.toggle("hover"); // lub .add("hover")
+// });
+
+// myButton.addEventListener("mouseout", event =>{
+//     event.target.classList.toggle("hover"); // lub .remove("hover")
+// });
+
+myButton.classList.add("enabled");
+
+myButton.addEventListener("click", event =>{
+    
+    if(event.target.classList.contains("disabled")){
+        event.target.textContent += "👹";
+    }
+    else{
+        event.target.classList.replace("enabled","disabled");
+    }
+    
+});
+
+// Podobne przykłady dla <h1>,<h2>,...,<button> itd.
