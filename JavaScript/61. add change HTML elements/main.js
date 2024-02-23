bfb81f6f983b62879e89c55aa11a16db459e798c
kamerais <kubickimarcin102@gmail.com>
2024-02-23 17:10:50 +0100

    Przykład pierwszy dodawania/edytowanie/usuwania elementów HTMLa

---
+++ JavaScript/61. add change HTML elements/main.js
@@ -0,0 +1,24 @@
+// --------------- Przykład 1 ---------------
+
+// Krok 1 stworzyć element
+// const newH1 = document.createElement("h1");
+// Krok 2 Dodać atrybuty/własności
+// newH1.textContent = "I like pizza";
+// newH1.id = "newH1";
+// newH1.style.color = "tomato";
+// newH1.style.textAlign = "center";
+// Krok 3 Dołączyć element do DOMa
+//document.body.append(newH1);
+//document.body.prepend(newH1);
+//document.getElementById("box1").append(newH1)
+//document.getElementById("box1").prepend(newH1);
+
+// const box2 = document.getElementById("box2");
+// document.body.insertBefore(newH1, box2);
+
+// const boxes = document.querySelectorAll(".box");
+// document.body.insertBefore(newH1, boxes[index]);
+// Usunąć element HTML
+// document.body.removeChild(newH1);
+
+
