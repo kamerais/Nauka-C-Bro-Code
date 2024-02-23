c1e982fa257cd141fe4e571c6eb47c8547a8d5e9
kamerais <kubickimarcin102@gmail.com>
2024-02-24 00:36:03 +0100

    Definicja JSON i przykład metod

---
+++ JavaScript/72. JSON files/main.js
@@ -0,0 +1,6 @@
+// JSON = (JavaScript Object Notation) format wymiany danych
+//        Używany do wymiany danych między serwerem, a aplikacją webową
+//        Pliki JSON {key:value} lub {value1, value2, value3}
+
+//        JSON.stringify() = konwertuje obiekt JS na JSON string
+//        JSON.parse() = konwertuje JSON string na obiekt JS
