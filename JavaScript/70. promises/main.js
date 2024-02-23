248242b54721f15a991d8051be3f90aaa1df7ad9
kamerais <kubickimarcin102@gmail.com>
2024-02-23 23:45:19 +0100

    Definicja obietnicy

---
+++ JavaScript/70. promises/main.js
@@ -0,0 +1,11 @@
+// Promise = Obiekt, który zarządza asynchronicznymi operacjami.
+//           Zawija Obiekt Obietnicy wokół {asynchronicznego kodu}
+//           "Obiecuję zwrócić wartość"
+//           OCZEKIWANIE -> ZATWIERDZONE lub ODRZUCONE
+//           new Promise((zatwierdzone, odrzucone) => {asynchroniczny kod})
+
+// ZRÓB TE CZYNNOŚCI W KOLEJNOŚĆI
+
+// 1. Wyjdź na spacer z psem
+// 2. Posprzątaj kuchnie
+// 3. Wynieś śmieci
