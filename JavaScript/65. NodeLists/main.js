b48aa71d28831e32ca0e1aa197f3c795fc6c6db7
kamerais <kubickimarcin102@gmail.com>
2024-02-23 22:08:46 +0100

    Przykłady zastosowań NodeLists

--- JavaScript/65. NodeLists/main.js
+++ JavaScript/65. NodeLists/main.js
@@ -3,3 +3,45 @@
 //            Podobne do tablic, ale nie (map, filter, reduce)
 //            NodeList nie będzie się update-ować dla automatycznie odzwierciedlonych zmian.
 
+let buttons = document.querySelectorAll(".myButtons");
+
+// DODAWAĆ WŁAŚCIWOŚCI HTML/CSS
+
+// buttons.forEach(button =>{
+//     button.style.backgroundColor = "green";
+//     buttons.textContent += "🙃";
+// });
+
+// CLICK event listener
+
+// buttons.forEach(button =>{
+//     button.addEventListener("click",event =>{
+//         event.target.style.backgroundColor = "tomato";
+//     });
+// });
+
+// MOUSEOVER + MOUSEOUT event listener
+
+// buttons.forEach(button =>{
+//     button.addEventListener("mouseover",event =>{
+//         event.target.style.backgroundColor = "tomato";
+//     });
+
+//     button.addEventListener("mouseout", event =>{
+//         event.target.style.backgroundColor = "blue";
+//     });
+// });
+
+// DODAWAĆ ELEMENT
+
+// const newButton = document.createElement("button");
+// newButton.textContent = "Button 5";
+// newButton.classList = "myButtons";
+// document.body.appendChild(newButton);
+
+// USUWAĆ ELEMENT
+// buttons.forEach(button =>{
+//     button.addEventListener("click",event =>{
+//         event.target.remove();
+//     });
+// });
