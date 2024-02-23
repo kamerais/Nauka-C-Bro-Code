e98b1099482d2c2bfee1708836a715ac755208ab
kamerais <kubickimarcin102@gmail.com>
2024-02-23 20:53:00 +0100

    Przykład mouse eventów i ich użycie

--- JavaScript/62. mouse events/main.js
+++ JavaScript/62. mouse events/main.js
@@ -1,3 +1,21 @@
 // eventListener = słucha po specjalną akcję do tworzenia interaktywnych stron internetowych
 //                 akcje: click, mouseover (najechać), mouseout (odjechać)
 //                 .addEventListener(event, callback)
+
+const myBox = document.getElementById("myBox");
+
+
+myBox.addEventListener("click", event =>{
+    event.target.style.backgroundColor = "tomato";
+    event.target.textContent = "OUCH 😒";
+});
+
+myBox.addEventListener("mouseover", event => {
+    event.target.style.backgroundColor = "yellow";
+    event.target.textContent = "Don't do it .👌";
+});
+
+myBox.addEventListener("mouseout", event =>{
+    event.target.style.backgroundColor = "lightgreen";
+    event.target.textContent = "Click me 😁";
+})
