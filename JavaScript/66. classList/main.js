07617e8126533b06529f968aaf88a2071b31aa84
kamerais <kubickimarcin102@gmail.com>
2024-02-23 22:12:13 +0100

    Definicja classList

---
+++ JavaScript/66. classList/main.js
@@ -0,0 +1,4 @@
+// classList = Własność elementu w JS używany do interakcji
+//             z elementem z listy klas (CSS classes)
+//             Pozwala na zrobienie reużywalnych klas dla wielu elementów
+//             przez twoją stronę internetową
