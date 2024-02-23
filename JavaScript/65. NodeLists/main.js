27802574f4d9c240fe29b87e9c9215673b2f4900
kamerais <kubickimarcin102@gmail.com>
2024-02-23 21:47:59 +0100

    Definicja NodeLists

---
+++ JavaScript/65. NodeLists/main.js
@@ -0,0 +1,5 @@
+// NodeList = Statyczna kolekcja elementów HTML przez (id, klasę, element)
+//            Może zostać stworzony poprzez użycie querySelectorAll()
+//            Podobne do tablic, ale nie (map, filter, reduce)
+//            NodeList nie będzie się update-ować dla automatycznie odzwierciedlonych zmian.
+
