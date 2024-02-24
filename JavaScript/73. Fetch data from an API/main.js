1488d9a7cbc20f7393ef009b9ce2c33b77869340
kamerais <kubickimarcin102@gmail.com>
2024-02-24 01:33:04 +0100

    Przykład jak stosować fetch i przykładowy program

--- JavaScript/73. Fetch data from an API/main.js
+++ JavaScript/73. Fetch data from an API/main.js
@@ -3,3 +3,40 @@
 //         Upraszcza asynchronicznie wydobywanie danych w JS i
 //         danych asynchronicznie poprzez sieć.
 //         fetch(url, (options))
+
+// fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
+//     .then(response => {
+//         if(!response.ok){
+//             throw new Error("Could not fetch response");
+//         }
+//         return response.json();
+//     })
+//     .then(data => console.log(data))
+//     .catch(error => console.error(error));
+
+// lub profesjonalniej
+
+async function fetchData() {
+
+    try
+    {
+        const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+        
+        if (!response.ok)
+        {
+            throw new Error("Could not fetch response");
+        }
+
+        const data = await response.json();
+        const pokemonSprite = data.sprites.front_default;
+        const imgElement = document.getElementById("pokemonSprite");
+
+        imgElement.src = pokemonSprite;
+        imgElement.style.display = "block";
+    }
+    catch(error)
+    {
+        console.error(error);
+    }
+}
